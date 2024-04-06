48a86ed676d473a85c74e0c349e36ffe9f61ad2f
anya96k <161941144+anya96k@users.noreply.github.com>
2024-04-06 10:49:34 +0530

    Add files via upload

---
+++ next line.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"\n\nenter the welcome text to next line:\n"<<endl;
+	cout<<"--------------------------------------\n"<<endl;
+	cout<<"welcome\n";
+	cout<<"boys"<<endl;
+	
+	return 0;
+}
