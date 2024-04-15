13d0791f7b2e10bd1aca431ec83cfb1094de1f23
anya96k <161941144+anya96k@users.noreply.github.com>
2024-04-15 15:29:31 +0530

    Add files via upload

---
+++ splitname.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()  
+
+{
+
+    string name,last;
+    
+	     cout<<"Enter Your Full Name: ";
+           cin>>name;
+              cin.get();
+                  getline(cin, last);
+                          cout<<"Your First Name: "<<name<<endl;
+                              cout<<"Your Last Name: "<<last<<endl;
+                                   return 0;
+}
